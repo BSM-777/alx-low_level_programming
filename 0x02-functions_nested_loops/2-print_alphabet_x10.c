155423ca265d976755cd18b3b36e12962261a259
BSM-77 <marouanebassim12@gmail.com>
2023-07-18 16:15:08 -0700

    New

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+  *print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
+  *followed by a new line
+  *Return:0
+  */
+void print_alphabet_x10(void)
+{
+        int i;
+        char a;
+
+        for (i = 1; i <= 10; i++)
+        {
+                for (a = 'a'; a <= 'z'; a++)
+                putchar(a);
+		putchar('\n');
+        }
+}
