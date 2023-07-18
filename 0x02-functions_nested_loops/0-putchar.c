529563e39496f38773874b9c3a7da7808fbcd3e4
BSM-77 <marouanebassim12@gmail.com>
2023-07-18 05:32:16 -0700

    New

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,13 @@
-#include <stdio.h>
+#include <stdio.h> 
+#include "main.h"
 
-int main(void) {
+/** * function_name - Print a string
+  *  
+  *Description: this function for print the string "_putchar"
+  * Return: 0 value
+  */
+int main()
+{
 	printf("_putchar\n");
-		return 0;
+	return 0;
 }
