3bc28a1b2e0b48f8df4a7f0bb6d4728228ca8ca0
BSM-77 <marouanebassim12@gmail.com>
2023-07-19 03:45:52 -0700

    New

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,5 +1,12 @@
 #include "main.h"
 
+/**
+ *positive_or_negtive - Testing is number positive or negative
+ *
+ * Return: positif if i>0
+ * negatif if n < 0
+ * is zero if i = 0
+ */
 void positive_or_negative(int i)
 {
 	if (i > 0)
