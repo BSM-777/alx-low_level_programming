6128bbf91222fa50863c537c0976f4aa275cfbcb
BSM-77 <marouanebassim12@gmail.com>
2023-07-18 17:27:41 -0700

    New

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -4,9 +4,8 @@
  *  print_sign - prints the sign of a number
  * @n: the int to check
  * Return: 1 and prints + if n is greater than zero
- * 0 and prints 0 if n is zero
- * -1 and prints - if n is less than zero
-
+ * Return: 0 and prints 0 if n is zero
+ * Return: -1 and prints - otherwise
  */
 int print_sign(int n)
 {
