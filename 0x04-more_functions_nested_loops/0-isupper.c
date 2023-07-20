b987aaccd1cfbfa7df5103427d2bfa98aadff862
BSM-77 <marouanebassim12@gmail.com>
2023-07-20 03:15:30 -0700

    New

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "0-main.c"
+
+/**
+ *_isupper - check if c is upper or not
+ * @c: the char to check it
+ * Return: 1 if c isupper, 0 Otherwise
+ */
+int _isupper(int c)
+{
+	int c;
+
+	if (c = 'A' && c = 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
