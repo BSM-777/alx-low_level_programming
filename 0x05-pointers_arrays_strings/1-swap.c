a2c52e24f46de5c154236144ec5c93c3f74a8eb7
BSM-77 <marouanebassim12@gmail.com>
2023-07-24 01:58:43 -0700

    New

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * swap_int - swap the valeu of two intigers
+ *
+ * @a: the first int
+ * @b: the secound int
+ */
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
