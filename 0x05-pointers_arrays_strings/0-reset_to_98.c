949aa82549081b0ed4ec4ef9281aeae328580ab0
BSM-77 <marouanebassim12@gmail.com>
2023-07-24 01:54:03 -0700

    New

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - updates the value of the variable
+ * the pointer points to to 98
+ *@n: n is the int to update
+ * Return: Always 0 .
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
