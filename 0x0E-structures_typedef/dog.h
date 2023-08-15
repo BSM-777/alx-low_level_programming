129d210c1e89987ca7585a062519a83c2c62785f
BSM-77 <marouanebassim12@gmail.com>
2023-08-15 10:04:35 -0700

    NeW

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ *struct dog - dog struct
+ *@name: name of the dog
+ *@age: age of the dog
+ *@owner: the name of the dog owner
+ *
+ *Description: just a dog struct
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
