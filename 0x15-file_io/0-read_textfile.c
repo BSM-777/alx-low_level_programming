20c1a07a8dd7053e9e59e44c489538ae9c30e251
BSM-777 <marouanebassim12@gmail.com>
2023-09-04 06:11:49 -0700

    New Update

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,61 +1,57 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <fcntl.h>
 
+/**
+ * read_textfile - Reads and prints the content of a text file.
+ * @filename: The name of the file to read.
+ * @letters: The number of letters to read and print.
+ *
+ * Return: The actual number of letters read and printed, or 0 on failure.
+ */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
+        int file_descriptor;
+        ssize_t bytes_read, bytes_written;
+        char *buffer;
+
 	if (filename == NULL)
-	{
 		return (0);
-	}
-
-	FILE *file = fopen(filename, "r");
-	if (file == NULL)
-	{
+	/* Open the file for reading */
+	file_descriptor = open(filename, O_RDONLY);
+	if (file_descriptor == -1)
 		return (0);
-	}
-	char *buffer = (char *)malloc(letters);
 
+	/* Allocate memory for the buffer */
+	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
 	{
-		fclose(file);
+		close(file_descriptor);
 		return (0);
 	}
 
-	ssize_t bytes_read = fread(buffer, 1, letters, file);
-
-	if (bytes_read <= 0)
+	/* Read up to 'letters' characters from the file */
+	bytes_read = read(file_descriptor, buffer, letters);
+	if (bytes_read == -1)
 	{
+		close(file_descriptor);
 		free(buffer);
-		fclose(file);
 		return (0);
 	}
 
-	ssize_t bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
-	if (bytes_written != bytes_read)
+	/* Write the read content to the standard output (STDOUT) */
+	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+	if (bytes_written == -1 || bytes_written != bytes_read)
 	{
+		close(file_descriptor);
 		free(buffer);
-		fclose(file);
 		return (0);
 	}
 
+	/* Clean up and close the file */
+	close(file_descriptor);
 	free(buffer);
-	fclose(file);
-	return bytes_written;
-}
-int main() {
-	const char *filename = "example.txt";
-	size_t letters_to_read = 100;  // Change this to the desired number of letters.
 
-	size_t result = read_textfile(filename, letters_to_read);
-	if (result == 0)
-	{
-   		fprintf(stderr, "Error reading or printing the file.\n");
-	}
-	else
-	{
-		printf("Successfully read and printed %zd letters.\n", result);
-	}
-
-	return (0);
+	return (bytes_written);
 }
